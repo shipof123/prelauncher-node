b6cc9ab4bac2182b76c43f66a4cdcc78f5ba73af
shipof123 <34876781+shipof123@users.noreply.github.com>
2019-03-31 13:37:33 -0400

    Update routes.js

--- config/routes.js
+++ config/routes.js
@@ -1,7 +1,7 @@
 var routes = [
 	{path: "/", controller: "pages_controller"},
 	{path: "/subscribers", controller: "subscribers_controller"},
-	{path: "/c29afd5f57ab4232e54156f66b833ac9460936579dd53cd602fe5805010adee6c0512f9b", controller: "prizes_controller"}
+	{path: "/prizes", controller: "prizes_controller"}
 ]
 
 exports.activate = function(app){
